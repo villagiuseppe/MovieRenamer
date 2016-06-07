da8e1869b7bf1feec3a8648d80df83a0d75de5f0
Giuseppe Villa <giuseppe.villa@gmail.com>
2016-06-07 12:02:10 +0200

    Check Paramameters,  close #2

--- MovieRenamer/Program.cs
+++ MovieRenamer/Program.cs
@@ -8,25 +8,48 @@ namespace MovieRenamer
 {
     class Program
     {
-        private static string _language = "en";
-
+        
         static void Main(string[] args)
         {
-            _language = Convert.ToString(ConfigurationManager.AppSettings["language"]);
-            while (string.IsNullOrWhiteSpace(_language))
+
+            #region Check Paramameters            
+            string language = Convert.ToString(ConfigurationManager.AppSettings["language"]);
+            while (string.IsNullOrWhiteSpace(language))
             {
                 Console.WriteLine("With which language to run search ? (two digit, es. en = english)");
-                _language = Console.ReadLine();
+                language = Console.ReadLine();
             }
 
-            var directory = new DirectoryInfo(ConfigurationManager.AppSettings["from"]);
+            string inputPath = ConfigurationManager.AppSettings["from"];
+            while (string.IsNullOrWhiteSpace(inputPath) || ! Directory.Exists(inputPath))
+            {
+                Console.WriteLine("Insert a valid input directory");
+                inputPath = Console.ReadLine();
+            }
+            ConfigurationManager.AppSettings.Set("from", inputPath);
+
+            string outputPath = ConfigurationManager.AppSettings["to"];
+            while (string.IsNullOrWhiteSpace(outputPath) || !Directory.Exists(outputPath))
+            {
+                Console.WriteLine("Insert a valid output directory ");
+                Console.WriteLine("(enter empty to set equal to \"input directory\")");
+                
+                outputPath = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(outputPath))
+                    outputPath = inputPath;
+            }
+            ConfigurationManager.AppSettings.Set("to", outputPath);
+            #endregion
+
+            var directory = new DirectoryInfo(inputPath);
+            
             var files = Enumerable.Empty<FileInfo>();
             files = files.Concat(directory.GetFiles("*.mp4"));
             files = files.Concat(directory.GetFiles("*.m4v"));
                        
             foreach (var file in files)
             {
-                var task = Renamer.HandleFile(file,_language);
+                var task = Renamer.HandleFile(file, language);
                 task.Wait();
             }
             Console.WriteLine("No More Files to rename!");
